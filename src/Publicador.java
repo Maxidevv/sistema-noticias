ad690944e70383b1edc2d2507ed62577d74c941f
matiasmoyanobustos <matiasmoyanobustos@gmail.com>
2026-06-14 15:39:59 -0300

    Update Publicador.java

--- src/Publicador.java
+++ src/Publicador.java
@@ -2,9 +2,15 @@
  * Un comentarista es responsable de realizar comentarios sobre una noticia
  * Este contrato establece el método que debe implementarse para publicar una noticia.
  * @author Moyano Bustos, Darrul, Moncada, Zalazar
+ * @version 1.1
 **/
 public interface Publicador {
-    
+
+    /**
+     * Realiza las acciones necesarias para publicar una noticia.
+     * Las clases que implementen esta interfaz definirán el destino 
+     * o medio de la publicación de una noticia
+     */
     void publicarNoticia(); 
     
 }
